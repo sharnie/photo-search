db72dec39ac73f8239c79357343d6b68b44f57b8
Sharnie Ivery <sharnieivery@gmail.com>
2014-05-11 12:35:35 -0400

    Modified search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,17 @@
 class SearchController < ApplicationController
   def index
   	access_token = "353749247.4f6bccc.e0ef76fa6bc54751994cf2ba58a3f7f2"
-		client = Instagram.client(:access_token => access_token)
-  	tags = params[:q] ? client.tag_search(params[:q]) : client.tag_search('chicken')
-  	@tag = tags.present? ? tags[0].name : 'chicken'
-		@results = client.tag_recent_media(@tag)
+    client = Instagram.client(access_token: access_token)
+    default_search = client.tag_search('chicken')
+
+    if params[:q]
+      search_query = client.tag_search(params[:q])
+      @tag = search_query.present? ? search_query : default_search
+    else
+      @tag = default_search
+    end
+
+    @tag = @tag.first.name
+    @results = client.tag_recent_media(@tag)
   end
 end
--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,9 +1,9 @@
 require "instagram"
 
 Instagram.configure do |config|
-
 	config.client_id = "73a37dc40e294594a83cb4ec50d9adcd"
-
-	config.access_token = "44cf05a853724a0bb89dbcd32fb6d669"
-
+	config.access_token = "353749247.4f6bccc.e0ef76fa6bc54751994cf2ba58a3f7f2"
 end
+
+CALLBACK_URL = "http://localhost:3000/oauth/callback"
+SCOPE = "likes+comments+relationships"
